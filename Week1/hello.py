6f5d7f8aca9acb0b028cd1779a0ea9c3d701bc4a
hkruse <hans@profkruse.net>
2024-01-18 10:50:03 -0500

    Week 1 hello world program added

---
+++ Week1/hello.py
@@ -0,0 +1,8 @@
+# Our first script - make sure python works and get the version information
+# Import the "sys" module (interface to system functions)
+import sys
+# Get the python version
+python_version = sys.version
+# Print a hello message and the version information
+print("Hello, I am python version",python_version)
+
