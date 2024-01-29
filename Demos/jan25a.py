29456ea61d48c6b73ff3232db50f4a19765b5b3f
HK <kruse@ohio.edu>
2024-01-29 16:34:16 -0500

    added jan25 file from class desktop

---
+++ Demos/jan25a.py
@@ -0,0 +1,42 @@
+colors = ["red","green","blue","yellow"]
+
+# for c in colors:
+for c in range(4):
+    print(c)
+print("done")
+
+# # c = len(colors)
+# # colors.append("orange")
+# colors.extend(("orange","black"))
+# # colors.remove("red")
+# # colors.reverse()
+# colors.sort()
+
+# print(colors)
+# numbers = (2,77,12.5)
+# c = min(numbers)
+# colors = ("red","green","blue","yellow")
+# # c=colors[1]
+# c = colors[1:3]
+# print(c)
+# a = [60]
+# b = a
+# a.append(31)
+
+# print(id(a),a)
+# print(id(b),b)
+
+# a = 60
+# b = a+1
+# print(id(a))
+# print(id(b))
+
+
+# a = 60
+# b = 31
+
+# # c = a%b
+# # c = a**2
+# c = max(a,b)
+
+# print(c)
