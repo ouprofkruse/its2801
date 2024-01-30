5fe1c3e1a282776289584c01a6ca3773162b984b
Hans Kruse office <kruse@ohio.edu>
2024-01-30 13:49:19 -0500

    Note for Feb 1

--- Demos/workspace.py
+++ Demos/workspace.py
@@ -1,25 +1,51 @@
 # Used for testing of small code snippets
-import turtle
-COLORS = ("red","lightblue","green","yellow","brown","cyan")
+import random
+# n = 0
+# while n<10:
+#     print(n)
+#     if n == 3:
+#         n+=2
+#         continue
+#     if n > 6:
+#         break
+#     n+=1
 
-def draw_dots(my_size,my_position):
-    # size=999
-    # print (size)
-    turtle.up()
-    turtle.goto(my_position)
-    turtle.dot(my_size,"red")
-    turtle.setheading(90)
-    turtle.forward(my_size)
-    turtle.dot(my_size,"lightblue")
-    return turtle.position()
-    # my_size=999
-    # my_position[0]=123
+n=0
+while True:
+    x = random.random()
+    if x < 0.25:
+        break
+    else:
+        n+=1
+print(n)
 
-size = 20
-pos=[0,0]
-last = draw_dots(size,pos)
-print(last)
-# pos=[50,100]
-# draw_dots(size,pos)
 
-turtle.exitonclick()
+
+
+
+
+
+# import turtle
+# COLORS = ("red","lightblue","green","yellow","brown","cyan")
+
+# def draw_dots(my_size,my_position):
+#     # size=999
+#     # print (size)
+#     turtle.up()
+#     turtle.goto(my_position)
+#     turtle.dot(my_size,"red")
+#     turtle.setheading(90)
+#     turtle.forward(my_size)
+#     turtle.dot(my_size,"lightblue")
+#     return turtle.position()
+#     # my_size=999
+#     # my_position[0]=123
+
+# size = 20
+# pos=[0,0]
+# last = draw_dots(size,pos)
+# print(last)
+# # pos=[50,100]
+# # draw_dots(size,pos)
+
+# turtle.exitonclick()
