fd41f0f941e0858ab29eeb5190a5f60b910a80b1
HK <kruse@ohio.edu>
2024-02-01 15:28:10 -0500

    End of class Feb 1

---
+++ Demos/feb01.py
@@ -0,0 +1,45 @@
+import random
+# COLORS = ("black","blue","green","brown2","darkmagenta","darkorange","red")
+# COLORS = ("black","blue","green")
+COLORS = ("red","blue","green","brown2","darkmagenta")
+c_len = len(COLORS)
+
+n = 0
+x = 0
+while x > 0.9:
+    n += 1
+    x = random.random()
+print(n)
+
+
+# n = 0
+# while n<c_len:
+#     print(COLORS[n])
+#     n+=1
+
+
+# for c in COLORS:
+#     print(c)
+
+
+
+# n = -1
+
+# if n >= 0 and n <= c_len -1:
+#     print(COLORS[n])
+
+
+# if c_len < 5:
+#     print("Only",c_len,"Colors")
+# if c_len == 5 and "blue" in COLORS:
+#     print("Blue!")
+# elif c_len == 5:
+#     print("Just enough colors")
+#     if "black" in COLORS:
+#         print("We have black")
+#     else:
+#         print("No Black")
+# else:
+#     print("Enough colors")
+
+# == > < >= <= != in
