a9e1587760b4e7297c4bcc872510b994c9b2b43f
hkruse <hans@profkruse.net>
2024-02-11 22:53:13 -0500

    Week 5 prep - files and string formatting

--- Demos/workspace.py
+++ Demos/workspace.py
@@ -1,21 +1,37 @@
 # Used for testing of small code snippets
-# import turtle
-# colors = ("red","blue","green","yellow","brown","cyan")
-colors = ["red","blue","green"]
+import turtle
 
-# colors.extend(("orange","black"))
-# for c in colors:
-#     print(c)
+def f():
+    turtle.forward(50)
+def l():
+    turtle.left(90)
 
-for i in range(2,10,2):
-    print(i)
+turtle.onkeypress(f,"w")
+turtle.onkeypress(l,"a")
 
-# numbers = (5,4.9,-15.3)
-# c = max(numbers)
+turtle.listen()
+turtle.mainloop()
 
-# a = 60
-# b = 31
+# data = input_file.read()
+# for i in range(4):
+# input_file = open("Demos/sample.txt")
+# while True:
+#     data = input_file.readline()
+#     if data == '':
+#         break
+#     print(">",data.strip())
+# for data in input_file:
+#     print(">",data.strip())
 
-# c = min(a,b)
+# input_file.close()
 
-# print(colors)
+# output_file = open("Demos/temp.txt",'a')
+# output_file.write("\nThis is yet another test")
+# output_file.write(["one","two","three"])
+
+# output_file.close()
+
+# x = 55.9
+# y = 3.1234567
+# s = f" x = {x:10.3f} \n y = {y:10.3f} "
+# print(s)
