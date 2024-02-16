abc0e06fe30fe0b837415bccdb14f9295b88719e
hkruse <hans@profkruse.net>
2024-02-15 21:34:38 -0500

    pygame - player class/object

--- pygame/Basics2.py
+++ pygame/Basics2.py
@@ -1,5 +1,7 @@
 import pygame
 import random
+# Constants
+V = .1
 
 def place_npc(npc_rect,screen_dim):
     x = random.random()*screen_dim[0]
@@ -7,14 +9,42 @@ def place_npc(npc_rect,screen_dim):
     npc_rect.center = (x,y)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image_file,screen_dim) -> None:
+    def __init__(self, image_file,the_screen) -> None:
         super().__init__()
+        self.scr_ref = the_screen
         self.original = pygame.image.load(image_file)
         self.image = pygame.transform.scale(self.original,(64,64))
         self.rect = self.image.get_rect()
+        screen_dim = self.scr_ref.get_size()
         self.rect.center = (screen_dim[0]/2,screen_dim[1]/2)
-    def update(self,screen):
-        screen.blit(self.image,self.rect)
+        self.velocity = V
+    def update(self,coll_rect):
+        if self.rect.colliderect(coll_rect):
+            screen_dim = self.scr_ref.get_size()
+            place_npc(coll_rect,screen_dim)
+        self.scr_ref.blit(self.image,self.rect)
+    def move(self,key_list,delta):
+        screen_dim = self.scr_ref.get_size()
+        increment = [0,0]
+        for i in range(2):
+            increment[i] = self.velocity * ((screen_dim[0]+screen_dim[1])/2) * delta
+        if key_list[pygame.K_w]:
+            self.rect.y -= increment[1]
+            if self.rect.y < 0:
+                self.rect.y = screen_dim[1]
+        if key_list[pygame.K_s]:
+            self.rect.y += increment[1]
+            if self.rect.y > screen_dim[1]:
+                self.rect.y = 0
+        if key_list[pygame.K_a]:
+            self.rect.x -= increment[0]
+            if self.rect.x < 0:
+                self.rect.x = screen_dim[0]
+        if key_list[pygame.K_d]:
+            self.rect.x += increment[0]
+            if self.rect.x > screen_dim[0]:
+                self.rect.x = 0
+
     
 pygame.init()
 #screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN, 0, 1)
@@ -22,11 +52,11 @@ screen = pygame.display.set_mode((1600,800))
 screen_size = screen.get_size()
 pygame.display.set_caption("ITS 2801")
 clock = pygame.time.Clock()
-V = .1
+
 
 #print(screen_size[1])
 
-player = Player("Images/eye.png",screen_size)
+player = Player("Images/eye.png",screen)
 
 hood_original = pygame.image.load("Images/hood.png")
 hood = pygame.transform.scale(hood_original,(64,64))
@@ -45,28 +75,12 @@ while running:
 #
     screen.fill("silver")
 
-    increment = [0,0]
-    for i in range(2):
-        increment[i] = V * ((screen_size[0]+screen_size[1])/2) * dt
-
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_q]:
+    if keys[pygame.K_ESCAPE]:
         running = False
-    if keys[pygame.K_w]:
-        player.rect.y -= increment[1]
-    if keys[pygame.K_s]:
-        player.rect.y += increment[1]
-    if keys[pygame.K_a]:
-        player.rect.x -= increment[0]
-    if keys[pygame.K_d]:
-        player.rect.x += increment[0]
-    if keys[pygame.K_l]:
-        print(screen_size,increment)
 
-    if player.rect.colliderect(hood_rect):
-        place_npc(hood_rect,screen_size)
-        
-    player.update(screen)
+    player.move(keys,dt)
+    player.update(hood_rect)
     screen.blit(hood,hood_rect)
     
     pygame.display.flip()
