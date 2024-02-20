77967fc4f06e925e25389d05eda1726dc40208e9
HK <kruse@ohio.edu>
2024-02-20 15:31:14 -0500

    end of class Tuesday

---
+++ Demos/feb20.py
@@ -0,0 +1,12 @@
+# L = [1,2,3]
+# print(L[1])
+
+d = {}
+d["John"] = 55
+d["Mary"] = 22
+
+print(d)
+if "John" in d.keys():
+    print(d["John"])
+
+
--- Demos/pygame-init.py
+++ Demos/pygame-init.py
@@ -1,16 +1,35 @@
 
 import pygame  # Our freshly installed pygame package
 import random  # Can't have a game without RNG...
+MC_IMAGE = "/home/itsvm/src/its2801/Week6/Images/hood64.png"
 
+def place_mc():
+    image = pygame.image.load(MC_IMAGE)
+    rect = image.get_rect()
+    rect.center = (SCREEN_SIZE[0]/2,SCREEN_SIZE[1]/2)
+    return [image,rect]
+
+def show_mc(image,rect):
+    screen.blit(image,rect)
 
 pygame.init()
 
 screen = pygame.display.set_mode((1600,800))
-
+SCREEN_SIZE = screen.get_size()
+mc_id = place_mc()
 # Every game action happens in an infite loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        running=False
+        
+    screen.fill("silver")
+    show_mc(mc_id[0],mc_id[1])
 
+    pygame.display.flip()
+
+pygame.quit()
