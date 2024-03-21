59aea89eceb0f993888480a592b0af0788ec4b9a
hkruse <hans@profkruse.net>
2024-03-21 19:55:41 -0400

    temp. commit for C# starting point - hello to be removed later

--- hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
+++ hello/obj/Debug/net8.0/hello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cd5bab6db7cb5fc5f053b74216413d07d8e4ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7192c18c2a11d817227da27cc3271147b8f59c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("hello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
