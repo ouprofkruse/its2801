15b1e4fee4f5da30bf1a65cb75ccea6bae5be5d4
hkruse <hans@profkruse.net>
2024-03-28 09:50:43 -0400

    March 28 class prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,17 +1,29 @@
-﻿// import math
-// location1 = [2,5]
-// location2 = [3,6]
-// # distance is square-root(x-distance-squared + y-distance-squared)
-// x_dist2 = (location1[0]-location2[0])**2
-// y_dist2 = (location1[1]-location2[1])**2
-// distance = math.sqrt(x_dist2+y_dist2)
-// print(distance)
+﻿string[] words = [" "];
+string? message;
+string empty = String.Empty;
+string empty2 = "";
+string? nullString  = null;
+Console.WriteLine(empty);
+Console.WriteLine(empty2);
+Console.WriteLine(nullString);
+Console.Write("> ");
+message = Console.ReadLine();
+if (message != null)
+{
+    words = message.Split();
+}
+else
+{
+    words = ["n/a"];
+}
+int nWords = words.Length;
+for (int i=0; i < nWords; i++)
+{
+    Console.WriteLine(words[i]);
+}
 
-long[] location1 = [2,5];
-long[] location2 = [3,6];
-
-double xDist2 = Math.Pow(location1[0]-location2[0],2);
-double yDist2 = Math.Pow(location1[1]-location2[1],2);
-
-double distance = Math.Sqrt(xDist2+yDist2);
-Console.WriteLine(distance);
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+Console.WriteLine(words[0]);
