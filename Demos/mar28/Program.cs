af49258cc4c4bbc4c1619bb2290f73b8dc8e507b
HK <kruse@ohio.edu>
2024-03-28 15:31:55 -0400

    Mar 28 class

---
+++ Demos/mar28/Program.cs
@@ -0,0 +1,24 @@
+﻿string? message = "Hello";
+//string message2 = " there";
+//string message = "abc";
+//string? nullString = null;
+Console.Write("> ");
+message = Console.ReadLine();
+string[] words = [];
+if (message != null && message != "")
+{
+    words = message.Split();
+}
+else
+{
+    words = ["n/a"];
+}
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+// Console.Write("Message: <");
+// Console.Write(words[0]);
+// Console.WriteLine(">");
