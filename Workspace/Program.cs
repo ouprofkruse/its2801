d3401e0189d36f899c6440790ca638b0a4ba8a06
hkruse <hans@profkruse.net>
2024-04-02 10:25:39 -0400

    April 2 class prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,29 +1,4 @@
-﻿string[] words = [" "];
-string? message;
-string empty = String.Empty;
-string empty2 = "";
-string? nullString  = null;
-Console.WriteLine(empty);
-Console.WriteLine(empty2);
-Console.WriteLine(nullString);
-Console.Write("> ");
-message = Console.ReadLine();
-if (message != null)
-{
-    words = message.Split();
-}
-else
-{
-    words = ["n/a"];
-}
-int nWords = words.Length;
-for (int i=0; i < nWords; i++)
-{
-    Console.WriteLine(words[i]);
-}
-
-foreach (string word in words)
-{
-    Console.WriteLine(word);
-}
-Console.WriteLine(words[0]);
+﻿double nInput = 1.83456;
+long iOutput = Convert.ToInt64(nInput);
+Console.WriteLine(nInput);
+Console.WriteLine(iOutput);
