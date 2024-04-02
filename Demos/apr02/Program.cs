0602ea81220b44829cebcabecf6fd6bddc9810b3
HK <kruse@ohio.edu>
2024-04-02 15:23:17 -0400

    April 2 class

---
+++ Demos/apr02/Program.cs
@@ -0,0 +1,57 @@
+﻿int i = 5;
+double x;
+x = 1.0/3.0;
+Console.WriteLine($"i = [{i,-10:D3}]");
+Console.WriteLine($"x = [{x,-10:F2}]");
+
+
+
+
+
+
+// string? userInput = Console.ReadLine();
+// char select = Convert.ToChar(userInput);
+// switch (select)
+// {
+//     case 'a':
+//     Console.WriteLine("alpha");
+//     break;
+//     case 'b':
+//     Console.WriteLine("beta");
+//     break;
+//     default:
+//     Console.WriteLine("Huh");
+//     break;
+// }
+
+
+
+
+
+
+
+
+// string strInput = "ABC";
+// char chrResult = Convert.ToChar(strInput[1]);
+// Console.WriteLine(chrResult);
+// double nInput = -1.83456;
+// long iOutput = Convert.ToInt64(nInput);
+// long iOutput = (long) nInput;
+// Console.WriteLine(Math.Floor(nInput));
+// Console.WriteLine(Math.Truncate(nInput));
+// Console.WriteLine(nInput);
+// Console.WriteLine(iOutput);
+
+// int n=5;
+// do 
+// {
+//     Console.WriteLine(n);
+//     n++;
+// } while(n < 5);
+
+
+// while (n < 5)
+// {
+//     Console.WriteLine(n);
+//     n++;
+// }
