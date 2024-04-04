ffc91c9e131d577f0671e591be893cf7d6239df4
HK <kruse@ohio.edu>
2024-04-04 15:19:07 -0400

    April 4 class

---
+++ Demos/apr04/Program.cs
@@ -0,0 +1,46 @@
+﻿static double cube(double x)
+{
+    return x*x*x;
+}
+
+Console.WriteLine(cube(3));
+
+
+// int[][] ar3 = new int[3][];
+// ar3[0] = [1,2,3];
+// ar3[1] = [5];
+// ar3[2] = [6,7];
+
+// Console.WriteLine(ar3[2][0]);
+
+// int[,] ar2 = new int[,] {{1,2,3},{4,5,6}};
+// int[,] ar2 = new int[2,3];
+// Console.WriteLine($"Rank: {ar2.Rank}");
+// Console.WriteLine($"Length: {ar2.Length}");
+// Console.Write("Dimension lengths: ");
+// for (int i = 0; i < ar2.Rank; i++)
+// {
+//     Console.Write(ar2.GetLength(i));
+//     Console.Write(" ... ");
+// }
+// Console.WriteLine("");
+
+// for (int y = 0; y < ar2.GetLength(1); y++)
+// {
+//     for (int x = 0; x < ar2.GetLength(0); x++)
+//     {
+//         Console.Write($"  {ar2[x,y],5}");
+//     }
+//     Console.WriteLine("");
+// }
+
+
+
+
+// int[] ar = [3,2,1,8,9];
+// int[] ar = new int[4];
+// ar[2] = 55;
+
+// Console.WriteLine(ar.Length);
+// Console.WriteLine(ar[2]);
+
