8393735d48f54ef0bd7345eba061a810e8443c52
HK <kruse@ohio.edu>
2024-04-09 15:19:48 -0400

    April 9 class

---
+++ Demos/apr09/Program.cs
@@ -0,0 +1,37 @@
+﻿static int[] location(Random random)
+{
+    int x = random.Next(10);
+    int y = random.Next(10);
+    return [x,y];
+}
+var rand = new Random();
+int[] ship = location(rand);
+Console.WriteLine($"Ship at {ship[0]} {ship[1]}");
+
+
+
+
+// static double Sum(double[] items)
+// {
+//     double tempSum = 0.0;
+//     for (int i=0; i < items.Length; i++)
+//     {
+//         tempSum += items[i];
+//     }
+//     return tempSum;
+// }
+// double[] list=[5,9,1,4.5];
+// Console.WriteLine(Sum(list));
+
+
+
+
+// var rand = new Random();
+// for (int i = 0; i<5; i++)
+// {
+//     // int number = rand.Next(5,10);
+//     // Console.Write($"{number,15:D}");
+//     double number = rand.NextDouble();
+//     Console.Write($"{number,15:F4}");
+// }
+// Console.WriteLine("");
