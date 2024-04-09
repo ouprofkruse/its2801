569e22d83c6c02b1821bfee7f94182082278ff6a
hkruse <hans@profkruse.net>
2024-04-09 13:04:13 -0400

    Week 13 prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,13 +1,3 @@
-﻿static void Matrix(int[,] data)
-{
-    for (int x = 0; x < data.GetLength(0); x++)
-    {
-        for (int y = 0; y < data.GetLength(1); y++)
-        {
-            Console.Write($"{data[x,y]}  ");
-        }
-        Console.WriteLine("");
-    }
-}
-int[,] input = new int[,] {{1,2},{3,4}};
-Matrix(input);
+﻿Ship enterprise = new Ship("Enterprise",5000);
+
+Console.WriteLine(enterprise.name);
---
+++ Workspace/ship.cs
@@ -0,0 +1,10 @@
+public class Ship
+{
+    private int _energy;
+    public string name;
+    public Ship(string shipName, int energy)
+    {
+        _energy = energy;
+        name = shipName;
+    }
+}
