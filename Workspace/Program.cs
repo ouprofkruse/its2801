2a939080ca0e4606cc50ee52a6633dcc37024310
hkruse <hans@profkruse.net>
2024-04-09 10:29:37 -0400

    Week 13 class prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,53 +1,13 @@
-﻿// int[] ar = new int[4];
-// Console.WriteLine(ar.Length);
-// Console.WriteLine(ar[1]);
-
-// int[] ar1 = [2,3,4];
-// Console.WriteLine(ar1.Length);
-// int[,] ar2 = new int[,] {{1,2,3},{4,5,6}};
-// Console.WriteLine(ar2.Rank);
-// Console.WriteLine(ar2.GetLength(0));
-// Console.WriteLine(ar2.GetLength(1));
-// Console.WriteLine(ar2[0,0]);
-// Console.WriteLine(ar2[0,1]);
-// Console.WriteLine(ar2[0,2]);
-// Console.WriteLine(ar2[1,0]);
-// Console.WriteLine(ar2[1,1]);
-// Console.WriteLine(ar2[1,2]);
-
-
-// int[,] ar3 = new int[2,3];
-// int n = 2;
-// for (int i1 = 0; i1 < ar3.GetLength(0); i1++)
-// {
-//     for (int i2 = 0; i2 < ar3.GetLength(1); i2++)
-//     {
-//         ar3[i1,i2] = n;
-//         n = n*2;
-//     }
-// }
-// Console.Write($" {ar3[0,0]}");
-// Console.Write($" {ar3[0,1]}");
-// Console.WriteLine($" {ar3[0,2]}");
-// Console.Write($" {ar3[1,0]}");
-// Console.Write($" {ar3[1,1]}");
-// Console.WriteLine($" {ar3[1,2]}");
-
-// int[][] ar4 = new int[3][];
-// ar4[0] = [1,2,3];
-// ar4[1] = [5];
-// ar4[2] = [6,7];
-
-// Console.WriteLine(ar4[2][0]);
-
-// static double cube (double x)
-// {
-//     return x*x*x;
-// }
-// Console.WriteLine(cube(3));
-
-
-Console.WriteLine(Cube.Volume(3));
-Console.WriteLine(Cube.Area(3));
-
-
+﻿static void Matrix(int[,] data)
+{
+    for (int x = 0; x < data.GetLength(0); x++)
+    {
+        for (int y = 0; y < data.GetLength(1); y++)
+        {
+            Console.Write($"{data[x,y]}  ");
+        }
+        Console.WriteLine("");
+    }
+}
+int[,] input = new int[,] {{1,2},{3,4}};
+Matrix(input);
