ccc11ab6ed29146e13fc3e9785ff090c98e2f17c
hkruse <hans@profkruse.net>
2024-04-11 12:46:12 -0400

    Week 13 (Thu) prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,3 +1,25 @@
-﻿Ship enterprise = new Ship("Enterprise",5000);
+﻿// Ship enterprise = new Ship("Enterprise",5000,10);
+// Console.WriteLine(enterprise.name);
+// enterprise.name = "NC 1701";
+// Console.WriteLine(enterprise.ShipID());
+// enterprise.Hit(1500);
+// Console.WriteLine(enterprise.ShipID());
+// enterprise.Torpedoes = 11;
+// Console.WriteLine(enterprise.Torpedoes);
 
-Console.WriteLine(enterprise.name);
+Coords location = new Coords(10,20);
+Console.WriteLine(location.X);
+Console.WriteLine(location.ToString());
+public struct Coords
+{
+    public Coords(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public double X { get; }
+    public double Y { get; }
+
+    public override string ToString() => $"({X}, {Y})";
+}
--- Workspace/ship.cs
+++ Workspace/ship.cs
@@ -1,10 +1,30 @@
 public class Ship
 {
     private int _energy;
+    private int _magazine;
+    private int _torpedoes = 0;
+    public int Torpedoes
+    {
+        get {return _torpedoes;}
+        set 
+        {
+            if (value > _magazine) _torpedoes=_magazine;
+            else _torpedoes = value;
+        }
+    }
     public string name;
-    public Ship(string shipName, int energy)
+    public Ship(string shipName, int energy, int magazine)
     {
         _energy = energy;
+        _magazine = magazine;
         name = shipName;
+    }
+    public string ShipID()
+    {
+        return $"{name} Shields at {_energy}";
+    }
+    public void Hit(int energy)
+    {
+        _energy = Math.Max(0,_energy - energy);
     }
 }
