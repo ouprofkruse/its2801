5fd3e1f92d4732b68c7ac009fc8e589eb7e13811
hkruse <hans@profkruse.net>
2024-04-23 12:08:20 -0400

    Week 15 assignment (final) and class prep

--- Workspace/Program.cs
+++ Workspace/Program.cs
@@ -1,32 +1,18 @@
-﻿// Ship enterprise = new Ship("Enterprise",5000,10);
-// Console.WriteLine(enterprise.name);
-// enterprise.name = "NC 1701";
-// Console.WriteLine(enterprise.ShipID());
-// enterprise.Hit(1500);
-// Console.WriteLine(enterprise.ShipID());
-// enterprise.Torpedoes = 11;
-// Console.WriteLine(enterprise.Torpedoes);
-
-Romulan flagship = new Romulan("T'Met",4000,12,false);
-Console.WriteLine(flagship.ShipID());
-// flagship.CloaKStatus();
-flagship.Cloak();
-// flagship.CloaKStatus();
-Console.WriteLine(flagship.ShipID());
-
-// Coords location = new Coords(10,20);
-// Console.WriteLine(location.X);
-// Console.WriteLine(location.ToString());
-// public struct Coords
-// {
-//     public Coords(double x, double y)
-//     {
-//         X = x;
-//         Y = y;
-//     }
-
-//     public double X { get; }
-//     public double Y { get; }
-
-//     public override string ToString() => $"({X}, {Y})";
-// }
+﻿string? answer;
+try
+{
+    Console.Write("> ");
+    answer = Console.ReadLine();
+    if (answer == null || answer == "")
+    {
+        throw new ArgumentOutOfRangeException(nameof(answer),"Answer cannot be empty");
+    }
+    else
+    {
+        Console.WriteLine(42/Convert.ToInt64(answer));
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Bad input, {e.Message}");
+}
